8ffc15fd8f285a332ff0e79dccbd4bb5e396351c
lucascabraldeve <lucas_cabral.deve@outlook.com>
2020-12-27 09:23:58 -0300

    Função update de tarefas configurada.

--- app/src/main/java/com/lucascabral/taskapplication/service/repository/TaskRepository.kt
+++ app/src/main/java/com/lucascabral/taskapplication/service/repository/TaskRepository.kt
@@ -69,6 +69,26 @@ class TaskRepository(val context: Context) {
         })
     }
 
+    fun update(task: TaskModel, listener: APIListener<Boolean>) {
+        val call: Call<Boolean> =
+                mRemote.update(task.id, task.priorityId, task.description, task.dueDate, task.complete)
+        call.enqueue(object : Callback<Boolean>{
+            override fun onResponse(call: Call<Boolean>, response: Response<Boolean>) {
+                if (response.code() != TaskConstants.HTTP.SUCCESS) {
+                    val validation =
+                            Gson().fromJson(response.errorBody()!!.string(), String::class.java)
+                    listener.onFailure(validation)
+                } else {
+                    response.body()?.let { listener.onSuccess(it) }
+                }
+            }
+
+            override fun onFailure(call: Call<Boolean>, t: Throwable) {
+                listener.onFailure(context.getString(R.string.ERROR_UNEXPECTED))
+            }
+        })
+    }
+
     fun load(id: Int, listener: APIListener<TaskModel>) {
         val call: Call<TaskModel> = mRemote.load(id)
         call.enqueue(object : Callback<TaskModel>{
--- app/src/main/java/com/lucascabral/taskapplication/viewmodel/TaskFormViewModel.kt
+++ app/src/main/java/com/lucascabral/taskapplication/viewmodel/TaskFormViewModel.kt
@@ -30,19 +30,32 @@ class TaskFormViewModel(application: Application) : AndroidViewModel(application
     }
 
     fun save(task: TaskModel) {
-        mTaskRepository.create(task, object : APIListener<Boolean>{
-            override fun onSuccess(model: Boolean) {
-                mValidation.value = ValidationListener()
-            }
 
-            override fun onFailure(str: String) {
-                mValidation.value = ValidationListener(str)
-            }
-        })
+        if (task.id == 0) {
+            mTaskRepository.create(task, object : APIListener<Boolean> {
+                override fun onSuccess(model: Boolean) {
+                    mValidation.value = ValidationListener()
+                }
+
+                override fun onFailure(str: String) {
+                    mValidation.value = ValidationListener(str)
+                }
+            })
+        } else {
+            mTaskRepository.update(task, object : APIListener<Boolean> {
+                override fun onSuccess(model: Boolean) {
+                    mValidation.value = ValidationListener()
+                }
+
+                override fun onFailure(str: String) {
+                    mValidation.value = ValidationListener(str)
+                }
+            })
+        }
     }
 
     fun load(id: Int) {
-        mTaskRepository.load(id, object : APIListener<TaskModel>{
+        mTaskRepository.load(id, object : APIListener<TaskModel> {
             override fun onSuccess(model: TaskModel) {
                 mTask.value = model
             }
